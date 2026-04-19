6cb1383f3bab79c536c5644d741bcd0ff5385e08
sheikhsaifali <saifkhan601@gmail.com>
2026-04-19 14:46:38 +0530

    Two Sum II Day1

---
+++ src/com/knightforge/dev/TwoSumII.java
@@ -0,0 +1,34 @@
+package com.knightforge.dev;
+
+public class TwoSumII
+{
+    // numbers = [2,7,11,15], target = 9
+    public int[] twoSum(int[] numbers, int target)
+    {
+        if(numbers.length == 0)
+        {
+            return null;
+        }
+        else
+        {
+            int start = 0, end = numbers.length-1, sum = 0;
+            while(start<end)
+            {
+                sum = numbers[start] + numbers[end];
+                if(sum == target)
+                {
+                    return new int[]{start+1, end+1};
+                }
+                else if(sum < target)
+                {
+                    start++;
+                }
+                else
+                {
+                    end--;
+                }
+            }
+        }
+        return null;
+    }
+}
