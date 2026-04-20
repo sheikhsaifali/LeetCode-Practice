0bb70cafe444ffa29e4cdcf78a17c88c9e9f0136
sheikhsaifali <saifkhan601@gmail.com>
2026-04-20 08:58:50 +0530

    Merge Two Sorted Array Using Two Pointer Day2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import com.knightforge.dev.MergeTwoArray;
 import com.knightforge.dev.RemoveDuplicatesArray;
 import com.knightforge.dev.TwoSumII;
 
@@ -9,5 +10,6 @@ public class Main {
         RemoveDuplicatesArray rda = new RemoveDuplicatesArray();
         Arrays.stream(twoSumII.twoSum(new int[]{2, 7, 11, 15}, 9)).forEach(System.out::println);
         System.out.println(rda.removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,41}));
+        new MergeTwoArray().merge(new int[]{1,2,3,0,0,0}, 3, new int[]{2,5,6}, 3);
     }
 }
--- src/com/knightforge/dev/MergeTwoArray.java
+++ src/com/knightforge/dev/MergeTwoArray.java
@@ -1,8 +1,39 @@
 package com.knightforge.dev;
 
+import java.util.Arrays;
+
 public class MergeTwoArray {
     public void merge(int[] nums1, int m, int[] nums2, int n)
     {
-
+        int[] pivot = new int[nums1.length];
+        int num1 = 0, num2 = 0, p = 0, r = 0;
+        while(nums1.length > num1 && n > num2)
+        {
+            if(nums1[num1] < nums2[num2] && m > num1)
+            {
+                pivot[p] = nums1[num1];
+                p++;
+                num1++;
+            }
+            else
+            {
+                pivot[p] = nums2[num2];
+                num2++;
+                p++;
+            }
+        }
+        while(nums1.length > num1 && m > num1)
+        {
+            pivot[p] = nums1[num1];
+            num1++;
+            p++;
+        }
+        while(p>0)
+        {
+            nums1[r] = pivot[r];
+            r++;
+            p--;
+        }
+        //Arrays.stream(pivot).forEach(System.out::println);
     }
 }
