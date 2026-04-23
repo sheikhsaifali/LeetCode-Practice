b97f7f8472467b6de727114b2b21747f5a20e054
sheikhsaifali <saifkhan601@gmail.com>
2026-04-23 22:06:39 +0530

    Square of Sorted Array Using Two Pointer Day3

---
+++ src/com/knightforge/dev/Three_Sum.java
@@ -0,0 +1,22 @@
+package com.knightforge.dev;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Three_Sum {
+
+    /*
+        Input: nums = [-1,0,1,2,-1,-4]
+        Output: [[-1,-1,2],[-1,0,1]]
+     */
+    public List<List<Integer>> threeSum(int[] nums)
+    {
+        int start = 0, mid = 0, end = nums.length;
+        List<List<Integer>> three = new ArrayList<>();
+        while(start < end)
+        {
+            //code here
+        }
+        return null;
+    }
+}
